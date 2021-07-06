dc38b441e1ebc7f5908041ccab05349798155c8a
chanu2757 <chanu2757@gmail.com>
2021-07-07 00:01:40 +0900

    21.07.06 오늘은 조금만

--- project/src/App.js
+++ project/src/App.js
@@ -6,29 +6,49 @@ import logo from './logo.svg';
 
 function App() {        
     //Array
-    let [글제목, 글제목변경] = useState(['남자 셔츠 추천','남자 반팔 추천']); 
-    let [작성날짜, 최종수정날짜] = useState(['21.07.01','21.07.05'])
-    let [조회수] = useState([20,10]);
+    let [글제목, 글제목변경] = useState(['남자 셔츠 추천','남자 반팔 추천','여름 신발 추천']); 
+    let [작성날짜, 최종수정날짜] = useState(['21.07.01','21.07.05','21.07.06'])
+    let [따봉, 따봉변경] = useState(0);
+    let [조회수,조회수변경] = useState(0);
     let main_content = " 메인 "    
+
+    function 조회수증가(){
+      조회수변경(조회수 + 1);      
+    }
+    
+    function 제목바꾸기(){
+      var newArray = [...글제목];      
+      newArray[0] = 글제목[1];
+      newArray[1] = 글제목[0];
+      newArray[2] = 글제목[2];
+      글제목변경( newArray );
+    }
+
   return (
-    <div className="App">
+    <div className="App">      
       <div className="nav_black">
          React 개발
       </div>
-
+     
       <div className="main_container">          
           <div className="main_Content">
                 { main_content }
           </div>
           <div className="side_list">
+          <button onClick={ 제목바꾸기 }>제목 정렬</button>
             <div className="side_list_index">
-                    <h3>{ 글제목[0] }</h3>
-                    <p>작성일 : { 작성날짜[0] } 조회수 : { 조회수[0] }</p>
+                    <h3 onClick={ ()=>{조회수증가( 조회수 + 1)} }>{ 글제목[0] }</h3>
+                    <p><span className="ddabong" onClick={ ()=>{ 따봉변경( 따봉 + 1 ) } }>👍 </span> {따봉}    작성일 : { 작성날짜[0] } 조회수 : { 조회수 } </p>
                     <hr/>
             </div>          
             <div className="side_list_index">
-                    <h3>{ 글제목[1] }</h3>
-                    <p>작성일 : { 작성날짜[1] } 조회수 : { 조회수[1] }</p>
+                    <h3>{ 글제목[1] } </h3>
+                    <p><span className="ddabong" onClick={ ()=>{console.log(1);}}> 👍 </span> {따봉}    작성일 : { 작성날짜[1] } 조회수 : { 1 }</p>
+                    <hr/>
+            </div>          
+            <div className="side_list_index">
+                    <h3>{ 글제목[1] } </h3>
+                    <p><span className="ddabong" onClick={ ()=>{console.log(1);}}> 👍 </span> {따봉}    작성일 : { 작성날짜[2] } 조회수 : { 1 }</p>
                     <hr/>
             </div>          
           </div>
